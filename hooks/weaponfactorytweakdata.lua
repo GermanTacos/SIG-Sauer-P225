842e4d9846296b7345dce2f1c2eb04a6662b8b93
IKillerwolfI <killerwolf.no1@gmail.com>
2019-01-20 05:45:52 +0100

    Gadget and Ext Positions

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -1,22 +1,19 @@
 Hooks:PostHook( WeaponFactoryTweakData, "init", "P220ModInit", function(self)
 	
-	self.wpn_fps_pis_noodle.adds = {
-		wpn_fps_upg_fl_pis_laser = {
-			"wpn_fps_pis_beretta_body_rail"
-		},
-		wpn_fps_upg_fl_pis_tlr1 = {
-			"wpn_fps_pis_beretta_body_rail"
-		},
-		wpn_fps_upg_fl_pis_crimson = {
-			"wpn_fps_pis_beretta_body_rail"
-		},
-		wpn_fps_upg_fl_pis_x400v = {
-			"wpn_fps_pis_beretta_body_rail"
-		},
-		wpn_fps_upg_fl_pis_m3x = {
+	self.wpn_fps_pis_noodle.adds.wpn_fps_upg_fl_pis_laser = {
 			"wpn_fps_pis_beretta_body_rail"
 		}
-	}
-	
+	self.wpn_fps_pis_noodle.adds.wpn_fps_upg_fl_pis_tlr1 = {
+			"wpn_fps_pis_beretta_body_rail"
+		}
+	self.wpn_fps_pis_noodle.adds.wpn_fps_upg_fl_pis_crimson = {
+			"wpn_fps_pis_beretta_body_rail"
+		}
+	self.wpn_fps_pis_noodle.adds.wpn_fps_upg_fl_pis_x400v = {
+			"wpn_fps_pis_beretta_body_rail"
+		}
+	self.wpn_fps_pis_noodle.adds.wpn_fps_upg_fl_pis_m3x = {
+			"wpn_fps_pis_beretta_body_rail"
+		}
 	
 end )
--- hooks/weapontweakdata.lua
+++ hooks/weapontweakdata.lua
@@ -1,34 +1,46 @@
-Hooks:PostHook( WeaponTweakData, "init", "Glock17modInit", function(self)
+Hooks:PostHook( WeaponTweakData, "init", "SigP220modInit", function(self)
 
-if ( self.glawk ) then
-	self:SetupAttachmentPoint("glawk", {
+if ( self.noodle ) then
+	self:SetupAttachmentPoint("noodle", {
 		name = "a_rds", 
         base_a_obj = "a_rds", 
-        position = Vector3( 0, -2.1, -0.4 ), 
+        position = Vector3( 0, -0.1, 0.4 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("glawk", {
+	self:SetupAttachmentPoint("noodle", {
 		name = "a_rds_sld", 
         base_a_obj = "a_rds", 
         position = Vector3( 0, -2.1, -0.7 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("glawk", {
+	self:SetupAttachmentPoint("noodle", {
 		name = "a_ns", 
         base_a_obj = "a_ns", 
-        position = Vector3( 0, -0.57, -0.65 ), 
+        position = Vector3( 0, 1, 0.1 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("glawk", {
+	self:SetupAttachmentPoint("noodle", {
 		name = "a_ns_ext", 
         base_a_obj = "a_ns", 
-        position = Vector3( 0, -0.3, -0.65 ), 
+        position = Vector3( 0, 1, 0 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
-	self:SetupAttachmentPoint("glawk", {
+	self:SetupAttachmentPoint( "noodle", {
+		name = "a_fl",
+		base_a_obj = "a_fl",
+		position = Vector3( 0, 1.5, -0.5 ), 
+        rotation = Rotation( 0, 0, 0 )
+		})
+	self:SetupAttachmentPoint( "noodle", {
+		name = "a_brtrail",
+		base_a_obj = "a_body",
+		position = Vector3( 0, -1, -0.5 ), 
+        rotation = Rotation( 0, 0, 0 )
+		}) 
+	self:SetupAttachmentPoint("noodle", {
 		name = "a_co", 
         base_a_obj = "a_co", 
-        position = Vector3( 0, -0.3, -0.65 ), 
+        position = Vector3( 0, 1.5, 0.2 ), 
         rotation = Rotation( 0, 0, 0 ) 
 	})
 end

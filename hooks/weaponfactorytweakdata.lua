73390b869739dfb053b1860de3fe65c223942959
GermanTacos <jakebuilds@gmail.com>
2019-01-24 04:43:45 -0500

    Removed all P220 stuff, fixed stainless parts, finished textures

--- hooks/weaponfactorytweakdata.lua
+++ hooks/weaponfactorytweakdata.lua
@@ -15,15 +15,5 @@ Hooks:PostHook( WeaponFactoryTweakData, "init", "P225ModInit", function(self)
 	self.wpn_fps_pis_sammy.adds.wpn_fps_upg_fl_pis_m3x = {
 			"wpn_fps_pis_beretta_body_rail"
 		}
-		
-	self.wpn_fps_pis_sammy.adds.wpn_fps_pis_sammy_fl_zom = {
-			"wpn_fps_pis_beretta_body_rail"
-		}
-		
-	for _, part_id in pairs(self.wpn_fps_pis_sammy.uses_parts) do
-		if self.parts[part_id].type == "barrel_ext" then
-			self.parts.wpn_fps_pis_sammy_sl_zom.override[part_id] = {a_obj="a_ns_zmb"}
-		end
-	end
 	
 end )
